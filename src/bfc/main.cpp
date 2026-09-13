4a4e1e6182f9a7d20754b831b491adff43f969e0
zibertscrem <zibertscrem@gmail.com>
2026-09-13 22:36:54 +0000

    Add primitive IR generation into main program via std in/out

--- src/bfc/main.cpp
+++ src/bfc/main.cpp
@@ -1,1 +1,22 @@
-int main() { return 0; }
+#include "bfc/core/lexer.hpp"
+#include "bfc/core/parser.hpp"
+#include "bfc/llvm/ir_generator.hpp"
+#include "bfc/llvm/ir_writer.hpp"
+
+#include <iostream>
+#include <llvm/IR/LLVMContext.h>
+#include <llvm/IR/Module.h>
+
+int main() {
+    bfc::lexer::Tokenizer tokenizer(std::cin);
+    bfc::parser::Parser parser(tokenizer);
+    auto program = parser.parse();
+
+    llvm::LLVMContext context;
+    llvm::Module module("brainfuck", context);
+    bfc::llvm::IRGenerator generator(module);
+    generator.generate(*program);
+
+    const bfc::llvm::IRWriter writer;
+    writer.write(module, std::cout);
+}
